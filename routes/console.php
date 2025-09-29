e50c8c52182bf4698aa747c283054a946dd5a5ba
ayunurhabibah <stevenle24ti@mahasiswa.pcr.ac.id>
2025-09-29 14:19:30 +0700

    Perubahan Console

--- routes/console.php
+++ routes/console.php
@@ -4,9 +4,7 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 
-
-
-//Semangat belajar
+// Cara Menjadi Orang Baik
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
